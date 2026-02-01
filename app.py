eead8133e1f4ef8d8cc4eef97b7f7853f4d3b2c8
Avijit Biswas <avijit4000@gmail.com>
2026-02-02 01:23:29 +0530

    new

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask on Azure!"
+    return "Hello from Avijit Biswas on Azure!"
 
 if __name__ == "__main__":
     app.run()
